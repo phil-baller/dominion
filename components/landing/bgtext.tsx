b021a9bccd9b6f6b84c943026acb20d0c8e91bde
code-env <org.codeen@gmail.com>
2023-10-21 14:35:01 +0100

    changes

--- components/landing/bgtext.tsx
+++ components/landing/bgtext.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const BgText = () => {
-  return <section>BgText</section>;
+  return (
+    <section className="p-20 bgtext h-[458px] flex items-center flex-col gap-4 justify-center">
+      <p className="text-5xl text-center font-semibold">
+        We can’t help everyone, but everyone can help <br /> someone
+      </p>
+
+      <button className="bg-primary py-3 px-10 w-fit">Donate Now</button>
+    </section>
+  );
 };
 
 export default BgText;
