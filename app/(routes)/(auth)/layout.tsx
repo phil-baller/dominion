a9ddd8d761e797e380c009f9754e55e2f73fdcb0
code-env <org.codeen@gmail.com>
2023-10-21 12:05:32 +0100

    changes in the auth layout

--- app/(routes)/(auth)/layout.tsx
+++ app/(routes)/(auth)/layout.tsx
@@ -1,7 +1,16 @@
+import Logo from "@/components/logo";
 import React, { ReactNode } from "react";
 
 const WhatWeDo = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="h-screen flex items-center justify-center flex-col gap-10">
+      <Logo />
+      <section className="w-[90%] h-[60%] rounded-lg p-10 lg:w-1/2 relative overflow-hidden">
+        <div className="absolute inset-0 bg-black/50 backdrop-blur-lg z-[-1]" />
+        {children}
+      </section>
+    </main>
+  );
 };
 
 export default WhatWeDo;
