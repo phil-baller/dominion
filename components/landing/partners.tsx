713b92cb141ee48bba533587b5ec4c135dba6268
code-env <org.codeen@gmail.com>
2023-10-21 17:42:19 +0100

    adding the partners sections

--- components/landing/partners.tsx
+++ components/landing/partners.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../header";
 
 const Partners = () => {
-  return <div>Partners</div>;
+  return (
+    <section className="bg-white text-[#111] p-20 flex gap-10 flex-col">
+      <Header content="Our Partners" />
+    </section>
+  );
 };
 
 export default Partners;
