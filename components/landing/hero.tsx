7a55805dae310531488536e135f89046c31f5394
code-env <org.codeen@gmail.com>
2023-10-21 12:41:14 +0100

    changed the font-family

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="hero py-20 flex flex-col gap-10">
+    <section className="hero py-32 flex flex-col gap-5">
       <p className="text-primary text-2xl">Together</p>
-      <h1 className="font-black text-[64px] leading-normal">
+      <h1 className="font-black text-[64px] leading-[70px]">
         Let’s make a <br /> difference
       </h1>
-      <p className="text-base ">
+      <p className="text-base">
         Lorem ipsum dolor sit amet consectetur. Sollicitudin tempor sit commodo{" "}
         <br />
         vitae est purus enim placerat venenatis. Risus auctor facilisi posuere{" "}
