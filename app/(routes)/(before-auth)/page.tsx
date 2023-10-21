07498113f7444a38f5c41621601dc0f53cafe816
code-env <org.codeen@gmail.com>
2023-10-21 13:47:38 +0100

    changed the or fixed the deployment error of the component that was not found

--- app/(routes)/(before-auth)/page.tsx
+++ app/(routes)/(before-auth)/page.tsx
@@ -1,10 +1,10 @@
-import Video from "@/components/landing/Video";
 import BgText from "@/components/landing/bgtext";
 import Blog from "@/components/landing/blog";
 import Family from "@/components/landing/family";
 import Hero from "@/components/landing/hero";
 import Mission from "@/components/landing/mission";
 import Partners from "@/components/landing/partners";
+import Video from "@/components/landing/video";
 import WhatweDo from "@/components/landing/what-we-do";
 import React from "react";
 
