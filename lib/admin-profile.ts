b1ce823a2e99fd74ffe0204bc03fd90bdc0abf19
code-env <org.codeen@gmail.com>
2023-10-30 19:39:59 +0100

    working on the authentication

---
+++ lib/admin-profile.ts
@@ -0,0 +1,26 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import { db } from "./db";
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) return redirectToSignIn();
+
+  const profile = await db.user.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) return profile;
+
+  const newProfile = await db.user.create({
+    data: {
+      userId: user.id,
+      username: `${user.firstName} ${user.lastName}`,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newProfile;
+};
