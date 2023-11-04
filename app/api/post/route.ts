6cbedacf2b9ec2204e6d0bbbc48fb63e40271c6b
code-env <org.codeen@gmail.com>
2023-11-04 14:10:14 +0100

    all

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,65 +1,65 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
 
-    const { userId }: { userId: string | null } = auth();
+//     const { userId }: { userId: string | null } = auth();
 
-    if (!userId)
-      return new NextResponse("Unauthorized!", {
-        status: 400,
-      });
-    //
+//     if (!userId)
+//       return new NextResponse("Unauthorized!", {
+//         status: 400,
+//       });
+//     //
 
-    const user = await db.user.findUnique({
-      where: {
-        userId: "sk_test_XG5tVr0LwHa3GOGZ1rK9K9YeW3UzKWNdIY2wdcYgAV",
-      },
-    });
+//     const user = await db.user.findUnique({
+//       where: {
+//         userId: "sk_test_XG5tVr0LwHa3GOGZ1rK9K9YeW3UzKWNdIY2wdcYgAV",
+//       },
+//     });
 
-    if (!user)
-      return new NextResponse("You don't have access in creating a post!", {
-        status: 400,
-      });
+//     if (!user)
+//       return new NextResponse("You don't have access in creating a post!", {
+//         status: 400,
+//       });
 
-    const { imageUrl, desc, userId: id } = body;
+//     const { imageUrl, desc, userId: id } = body;
 
-    if (!imageUrl || !desc || !userId) {
-      return new NextResponse("These fields are requuired", {
-        status: 401,
-      });
-    }
+//     if (!imageUrl || !desc || !userId) {
+//       return new NextResponse("These fields are requuired", {
+//         status: 401,
+//       });
+//     }
 
-    if (user.userId !== id)
-      return new NextResponse("You don't have access in creating a post!", {
-        status: 400,
-      });
+//     if (user.userId !== id)
+//       return new NextResponse("You don't have access in creating a post!", {
+//         status: 400,
+//       });
 
-    const post = await db.post.create({
-      data: {
-        desc,
-        imageUrl,
-        userId: user.userId,
-      },
-    });
+//     const post = await db.post.create({
+//       data: {
+//         desc,
+//         imageUrl,
+//         userId: user.userId,
+//       },
+//     });
 
-    //
-    return NextResponse.json({
-      msg: "Created succesfully",
-      post,
-    });
+//     //
+//     return NextResponse.json({
+//       msg: "Created succesfully",
+//       post,
+//     });
 
-    //
-  } catch (error: any) {
-    console.log("ERROR WHILE CREATING POST", error.message);
-    return new NextResponse("Internal server error", {
-      status: 500,
-    });
-  }
-}
+//     //
+//   } catch (error: any) {
+//     console.log("ERROR WHILE CREATING POST", error.message);
+//     return new NextResponse("Internal server error", {
+//       status: 500,
+//     });
+//   }
+// }
 
 export async function GET(req: Request) {
   try {
--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,21 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { getAuth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 // Set the paths that don't require the user to be signed in
-const publicPaths = [
-  "/",
-  "/sign-in*",
-  "/api/:path*",
-  "/sign-up*",
-  "/blog",
-  "/about",
-  "/what-we-do",
-  "/contact",
-];
 
-const isPublic = (path: string) => {
-  return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
-  );
-};
-
-export default withClerkMiddleware((request: NextRequest) => {
-  if (isPublic(request.nextUrl.pathname)) {
-    return NextResponse.next();
-  }
-  // if the user is not signed in redirect them to the sign in page.
-  const { userId } = getAuth(request);
-
-  if (!userId) {
-    // redirect the users to /pages/sign-in/[[...index]].ts
-
-    const signInUrl = new URL("/sign-in", request.url);
-    signInUrl.searchParams.set("redirect_url", request.url);
-    return NextResponse.redirect(signInUrl);
-  }
-  return NextResponse.next();
+export default authMiddleware({
+  publicRoutes: [
+    "/api/:path*",
+    "/",
+    "/sign-in*",
+    "/api/*",
+    "/sign-up*",
+    "/blog",
+    "/about",
+    "/what-we-do",
+    "/contact",
+  ],
 });
 
 export const config = {
-  matcher: ["/((?!static|.*\\..*|_next|favicon.ico).*)", "/"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
