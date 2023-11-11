2940eea3ebbeb94f4f870003a95ec74f9332f442
code-env <org.codeen@gmail.com>
2023-11-11 14:18:46 +0100

    all changes

--- components/header.tsx
+++ components/header.tsx
@@ -7,11 +7,12 @@ interface headerProps {
   content: string;
   spanClass?: string;
 }
+
 const Header: FC<headerProps> = ({ isSpan, spanText, spanClass, content }) => {
   return (
     <p className="lg:text-4xl text-2xl self-center relative">
       {content} {isSpan && <span className={cn(spanClass)}>{spanText}</span>}
-      <span className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-[107px] h-[3px] bg-primary"></span>
+      <span className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-[107px] h-[3px] bg-primary" />
     </p>
   );
 };
