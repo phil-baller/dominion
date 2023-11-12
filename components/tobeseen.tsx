3eacc510ea80b95bb30df7f86892fb60b96f325a
code-env <org.codeen@gmail.com>
2023-11-12 14:31:19 +0100

    working on the images and post optimization'

--- components/tobeseen.tsx
+++ components/tobeseen.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { postData } from "@/lib/reactquerry";
-import { Skeleton } from "./ui/skeleton";
 import PostComponent from "./landing/post";
 import { Post } from "@prisma/client";
 import PostSkeleton from "./skeletons/postSkeleton";
@@ -14,7 +13,7 @@ export const TobeSeen = () => {
     return (
       <section className="grid lg:grid-cols-3 grid-cols-1 gap-10 max-w-7xl w-full mx-auto">
         {Array.from({ length: 6 }).map((_, index) => (
-          <PostSkeleton />
+          <PostSkeleton key={index} />
         ))}
       </section>
     );
@@ -25,8 +24,8 @@ export const TobeSeen = () => {
   } else {
     return (
       <section className="grid lg:grid-cols-3 grid-cols-1 gap-10 max-w-7xl w-full mx-auto">
-        {posts.data.map((post: Post, index: number) => (
-          <PostComponent post={post} key={index} />
+        {posts.data.map((post: Post) => (
+          <PostComponent post={post} key={post.id} />
         ))}
       </section>
     );
