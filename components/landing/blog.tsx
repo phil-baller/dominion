b1fd1320ae20ad0aca1392e6f7d0c50a5cc9dcbd
code-env <org.codeen@gmail.com>
2023-11-12 15:27:08 +0100

    fixing some bugs

--- components/landing/blog.tsx
+++ components/landing/blog.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Header from "../header";
-import Post from "./post";
+import { TobeSeen } from "../tobeseen";
 
 const Blog = ({ isHeader }: { isHeader?: boolean }) => {
   return (
     <section className="bg-white text-[#111] padding flex gap-10 flex-col">
       {isHeader && <Header content="What we've done" />}
-      <section className="grid lg:grid-cols-3 grid-cols-1  gap-10 max-w-7xl w-full mx-auto">
-        {Array.from({ length: 6 }).map((_, index) => (
-          <Post index={index} key={index} />
-        ))}
-      </section>
+      <TobeSeen />
     </section>
   );
 };
