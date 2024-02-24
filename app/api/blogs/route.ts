a9e282c7d4eaf563750e74038c583eb19d6527fd
code-env <org.codeen@gmail.com>
2024-02-24 13:34:04 +0100

    feat: changing some content on the site:

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,10 +1,12 @@
 import { getCashedBlogs } from "@/constants";
 import { blogType } from "@/types";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   try {
     const blogs = await getCashedBlogs();
+    revalidatePath("/");
 
     const content = blogs as blogType[];
 
--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,10 +1,12 @@
 import { getCashedPictures } from "@/constants";
 import { imageType } from "@/types";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   try {
     const pictures = await getCashedPictures();
+    revalidatePath("/");
 
     const content = pictures as imageType[];
 
