713c7c97b175c228994a0b29255f8b8eea8cf9ef
Skaleway <140396134+skalewayteam@users.noreply.github.com>
2024-05-06 17:22:38 +0000

    feat: added the next.config.mjs file

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "via.placeholder.com",
+        pathname: "**",
+        protocol: "https",
+      },
+      {
+        hostname: "utfs.io",
+        protocol: "https",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+
+export default nextConfig;
